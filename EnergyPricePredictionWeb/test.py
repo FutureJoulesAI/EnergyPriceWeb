02191b4689db33e460efa173104f12cfa23d33f7
Quantal Software <projects@quantalsoftware.com.au>
2018-06-12 07:35:03 +1000

    Initial Commit

---
+++ EnergyPricePredictionWeb/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+def testFunc():
+    #print("This worked")
+    return "This worked"
+
+def main():
+    print(testFunc())
+    
+
+if __name__ == '__main__':
+    testFunc()
